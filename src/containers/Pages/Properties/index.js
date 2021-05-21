45d3cd5adf0aa636741a0d79208c11bd648baf7b
Andrius <andrius.rochalazarino@gmail.com>
2021-05-21 13:40:35 -0400

    item styled

--- src/containers/Pages/Properties/index.js
+++ src/containers/Pages/Properties/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { HeaderValues } from "../../providers/HeaderValues";
 import imoveis from "../../imoveis/imoveis.json"
 import imagem from "../../imoveis/imovel61.jpg"
-import { ImagePropertie2, ImagePropertie, InfoWrapper, PropertiesWrapper } from "./style"
+import {Division, PItem, SelectOrder, ImagePropertie, InfoWrapper, PropertiesWrapper, Wrapper, Title, PropertiesLength, ButtonOrder, TitleItem, DescriptionItem, ItemWrapper } from "./style"
 
 function Properties() {
   const { headerValues, setHeaderValues } = HeaderValues()
@@ -18,34 +18,41 @@ function Properties() {
   console.log(imoveis)
 
   return (
-    <div>
+    <Wrapper>
       <InfoWrapper>
         <div>
-          <h2>Todos os imóveis</h2>
-          <h3>6 imóveis</h3>
+          <Title>Todos os imóveis</Title>
+          <PropertiesLength>6 imóveis</PropertiesLength>
         </div>
-        <button>Ordenar</button>
+        <SelectOrder>
+          <option>
+            Ordenar
+          </option>
+        </SelectOrder>
       </InfoWrapper>
       <PropertiesWrapper>
         {
           imoveis.map((propertie, index) => {
             return (
-              <Link to="/propriedades/detalhe">
-                <div key={propertie.name}>
-                  <ImagePropertie src={propertie.image} />
-                  <h2>{propertie.name}</h2>
-                  <h3>{propertie.description}</h3>
-                  <p>{propertie.maxGuests}</p>
-                  <p>{propertie.cleaningFee}</p>
-                  <p>{propertie.guarantee}</p>
-                  <p>{propertie.district}</p>
-                </div>
-              </Link>
+              <ItemWrapper>
+                <Link to="/propriedades/detalhe">
+                  <ImagePropertie src={propertie.image} key={propertie.name} />
+                  <TitleItem>{propertie.name}</TitleItem>
+                  <DescriptionItem>{propertie.description}</DescriptionItem>
+                  <PItem><p>Máximo de hóspedes</p><p>{propertie.maxGuests}</p></PItem>
+                  <Division />
+                  <PItem><p>Taxa de limmpeza</p><p>R${propertie.cleaningFee.toFixed(2)}</p></PItem>
+                  <Division />
+                  <PItem><p>Caução</p><p>R${propertie.guarantee.toFixed(2)}</p></PItem>
+                  <Division />
+                  <PItem><p>Bairro</p><p>{propertie.district}</p></PItem>
+                </Link>
+              </ItemWrapper>
             )
           })
         }
       </PropertiesWrapper>
-    </div>
+    </Wrapper>
   );
 }
 
--- src/containers/Pages/Properties/style.js
+++ src/containers/Pages/Properties/style.js
@@ -1,25 +1,81 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 export const InfoWrapper = styled.div`
     display: flex;
     justify-content: space-between;
-    /* justify-self: c; */
+    align-self: center;
+    margin-top: 14px;
     h2 {
 
     }
     @media (min-width: 1024px) {
-        width: 1024px;
+      width: 1024px;
     }
 `
 export const ImagePropertie = styled.img`
   object-fit: none;
   object-position: center;
   height: 100px;
-  width: 320px;
+  width: 310px;
   border-radius: 9px;
-`
+ `
 export const PropertiesWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-self: center;
+  a:link{
+    text-decoration: none;
+  }
+  @media (min-width: 1024px) {
+    width: 1024px;
+  }
+`
+export const Title = styled.h2`
+  margin: 0 0 12px 0;
+  font-weight: 700;
+`
+export const PropertiesLength = styled.p`
+    color: #828787;
+    margin-top: 0;
+`
+export const SelectOrder = styled.select`
+    padding: 12px 24px 12px 7px;
+    width: fit-content;
+    height: fit-content;
+    border-radius: 10px;
+    border-color: #E0E1E1;
+    color: #828787;
+    font-weight: 700;
+`
+export const TitleItem = styled.h3`
+  color: #2a2d33;
+  font-weight: 600;
+`
+export const DescriptionItem = styled.p`
+  color: #2a2d33;
+  font-size: 10pt;
+  font-weight: 400;
+  margin-top: 0;
+`
+export const  PItem = styled.div`
+  color: #2a2d33;
+  font-size: 11pt;
+  display: flex;
+  justify-content: space-between;
+  p {
+    margin: 0;
+  }
+`
+export const Division = styled.div`
+    border-bottom: 1px dotted #E0E1E1;
+    width: 100%;
+    margin: 6px 0 6px 0;
+`
+export const ItemWrapper = styled.div`
+  margin: 12px 0 56px 0;
 `
